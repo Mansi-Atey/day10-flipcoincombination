38e7c69218e1b42f66d823c3993a975a2cd42102
unknown <you@example.com>
2021-02-19 17:56:30 +0530

    Added: percentage of singlet combination

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -4,7 +4,27 @@ tail=0
 randomNumber=$(($RANDOM%2))
 if [ $randomNumber -eq 1 ]
 then
-  echo "The winner is Heads"
+  echo "Heads"
 else
- echo "The winner is Tails"
+ echo "Tails"
 fi
+
+declare -A record
+totalHead=0
+totalTail=0
+
+for ((i=1;i<=20;i++))
+do
+ randomNumber=$(($RANDOM%2))
+if [ $randomNumber -eq 1 ]
+then
+ totalHead=$(($totalHead+1))
+else
+totalTail=$(($totalTail+1))
+fi
+done
+percentHead=$(($totalHead*5))
+percentTail=$(($totalTail*5))
+record[H]=$percentHead
+record[T]=$percentTail
+
